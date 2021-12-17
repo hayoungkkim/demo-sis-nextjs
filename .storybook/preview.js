a3abf14fbeba2050b14c14e3c1a8ffb8452ee05d
hayoungkkim <glorious_3@naver.com>
2021-12-17 15:52:51 +0900

    storybook 추가, Login 페이지 추가

---
+++ .storybook/preview.js
@@ -0,0 +1,21 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "../src/theme";
+
+export const decorators = [
+	(Story) => (
+		<ChakraProvider resetCSS theme={theme}>
+			<Story />
+		</ChakraProvider>
+	),
+];
+
+export const parameters = {
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
