eb482b5a3373021c0d84229c57cf07f80a58786d
hayoungkkim <glorious_3@naver.com>
2021-12-24 14:40:28 +0900

    Update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,72 @@
-const path = require("path");
+const path = require('path')
+const fs = require('fs')
+const { merge } = require('webpack-merge')
 
-const toPath = (_path) => path.join(process.cwd(), _path);
+function getPackageDir(filepath) {
+  let currDir = path.dirname(require.resolve(filepath))
+  while (true) {
+    if (fs.existsSync(path.join(currDir, 'package.json'))) {
+      return currDir
+    }
+    const { dir, root } = path.parse(currDir)
+    if (dir === root) {
+      throw new Error(
+        `Could not find package.json in the parent directories starting from ${filepath}.`
+      )
+    }
+    currDir = dir
+  }
+}
 
 module.exports = {
-	stories: ["../src/components/**/*.stories.mdx", "../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
-	addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-	typescript: {
-		reactDocgen: false,
-	},
-	webpackFinal: async (config) => {
-		return {
-			...config,
-			resolve: {
-				...config.resolve,
-				alias: {
-					...config.resolve.alias,
-					"@emotion/core": toPath("node_modules/@emotion/react"),
-					"emotion-theming": toPath("node_modules/@emotion/react"),
-				},
-			},
-		};
-	},
-};
+  core: {
+    builder: 'webpack5'
+  },
+  stories: [
+    '../src/components/**/*.stories.mdx',
+    '../src/components/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-links',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        docs: false
+      }
+    },
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true
+      }
+    },
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          include: [path.resolve(__dirname, '../src')]
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false }
+        }
+      }
+    }
+  ],
+  typescript: {
+    // also valid 'react-docgen-typescript' | false
+    reactDocgen: 'react-docgen'
+  },
+  refs: {
+    '@chakra-ui/react': { disable: true }
+  },
+  webpackFinal: async (config) => {
+    return merge(config, {
+      resolve: {
+        alias: {
+          '@emotion/core': getPackageDir('@emotion/react'),
+          'emotion-theming': getPackageDir('@emotion/react')
+        }
+      }
+    })
+  }
+}
