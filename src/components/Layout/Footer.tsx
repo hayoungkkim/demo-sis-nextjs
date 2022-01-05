e4e5ed55ff9690997de4f12b862b9527cf9b7b33
193664 <193664@ssg.com>
2022-01-05 15:14:21 +0900

    footer 수정

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,38 +1,46 @@
 import { Box, Text, Flex, Stack, Link } from '@chakra-ui/react'
+import { LogoIcon } from '../icons'
 
 interface FootItem {
-  copr_dt?: string
-  copr_if: string
-  copr_lk?: string
+  idx: number
+  name?: string
+  info: string
 }
 
 const FOOTER_ITEMS: Array<FootItem> = [
   {
-    copr_dt: '대표자',
-    copr_if: '강희석'
+    idx: 1,
+    name: '대표자',
+    info: '강희석'
   },
   {
-    copr_dt: '사업자등록번호',
-    copr_if: '870-88-01143'
+    idx: 2,
+    name: '사업자등록번호',
+    info: '870-88-01143'
   },
   {
-    copr_dt: '통신판매업 신고번호',
-    copr_if: '제2020-서울종로-1154호'
+    idx: 3,
+    name: '통신판매업 신고번호',
+    info: '제2020-서울종로-1154호'
   },
   {
-    copr_dt: '개인정보보호책임자',
-    copr_if: '김우진'
+    idx: 4,
+    name: '개인정보보호책임자',
+    info: '김우진'
   },
   {
-    copr_dt: '주소',
-    copr_if: '서울특별시 종로구 우정국로 26'
+    idx: 5,
+    name: '주소',
+    info: '서울특별시 종로구 우정국로 26'
   },
   {
-    copr_dt: 'Fax',
-    copr_if: '02-2068-7118'
+    idx: 6,
+    name: 'Fax',
+    info: '02-2068-7118'
   },
   {
-    copr_if: 'ssg@ssg.com'
+    idx: 7,
+    info: 'ssg@ssg.com'
   }
 ]
 
@@ -41,21 +49,16 @@ const Footer = () => {
     <Box bg="gray.200" ml={{ base: 0, md: 60 }} py={{ base: 7, md: 7 }} pl={{ base: 8, md: 12 }}>
       <Box maxW={{ base: 'full', md: '768px' }}>
         <Stack>
-          <Text fontSize="md" fontWeight="bold">
+          <Text fontSize="md" fontWeight="bold" mb={-5}>
             (주)에스에스지닷컴
           </Text>
-        </Stack>
-
-        <Stack>
-          <Text fontSize="7xl" fontWeight="bold">
-            SSG.COM
-          </Text>
+          <LogoIcon icon="logo_ssg" height={20} />
         </Stack>
 
         <Stack>
           <Flex flexFlow="wrap">
             {FOOTER_ITEMS.map((footItem) => (
-              <FootItem key={footItem.copr_dt} {...footItem} />
+              <FootItem key={footItem.idx} {...footItem} />
             ))}
           </Flex>
         </Stack>
@@ -84,11 +87,11 @@ const Footer = () => {
   )
 }
 
-const FootItem = ({ copr_dt, copr_if }: FootItem) => {
+const FootItem = ({ name, info }: FootItem) => {
   return (
-    <Stack key={copr_dt} mr={4}>
+    <Stack key={name} mr={4}>
       <Text color="gray.500" fontSize="sm">
-        {copr_dt} {copr_dt && ':'} {copr_if}
+        {name} {name && ':'} {info}
       </Text>
     </Stack>
   )
